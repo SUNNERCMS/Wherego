5d77a2b7a3b05c5d7b33e2e25238ac4476338c74
SUNNERCMS <309355484@qq.com>
2018-07-26 00:40:09 +0800

    merge from index-recommend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,26 @@
     <div>
        <home-header></home-header>
        <home-swiper></home-swiper>
+       <home-icons></home-icons>
+       <home-recommend></home-recommend>
+       <home-weekend></home-weekend>
     </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header.vue'
 import HomeSwiper from './components/Swiper.vue'
+import HomeIcons from './components/icons.vue'
+import HomeRecommend from './components/Recommend.vue'
+import HomeWeekend from './components/Weekend.vue'
 export default{
   name: 'Home',
   components: {
     HomeHeader: HomeHeader,
-    HomeSwiper: HomeSwiper
+    HomeSwiper: HomeSwiper,
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
   }
 }
 
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl" />
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_140x140_73fda71d.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0002',
+        imgUrl: 'https://imgs.qunarzz.com/p/tts8/1706/7c/026782d9dc37be02.jpg_r_390x260x90_debc208c.jpg',
+        title: '独特的新马异域风情',
+        desc: '双国新马6/5天之旅：武汉直飞，升级一晚国际五星，2人同行送保险'
+      }, {
+        id: '0003',
+        imgUrl: 'https://imgs.qunarzz.com/p/tts4/1804/fa/895d6b82a9a2ae02.jpg_262x238_6f1b2c9f.jpg',
+        title: '武汉宜昌-三峡人家',
+        desc: '小船悠悠，湖面粼粼，享受大自然的馈赠'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      overflow: hidden
+      // min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+</style>
